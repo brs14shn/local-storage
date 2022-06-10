9df329ad6d749ea0be106333671ba3fdb2274ea9
brs14shn <brsshn292929@gmail.com>
2022-06-10 19:47:09 +0300

    app created

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 //local storage:Browserin içindeki local depolama alanı;
 //dark-mode ==>
 // array içerisine depolayacağız.
+
+
+const form=document.querySelector(".todo_form");
+
+const input=document.querySelector(".todo_input");
+
+
+//
